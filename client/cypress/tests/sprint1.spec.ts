e045ce07dfe7b904eebeb358daae156ae2dabb57
Jefferson Van Buskirk <jef@lehigh.edu>
2021-01-03 23:19:52 -0500

    test?

--- client/cypress/tests/sprint1.spec.ts
+++ client/cypress/tests/sprint1.spec.ts
@@ -1,6 +1,18 @@
 describe('Sprint 1', () => {
-    // Before running each test, navigate to the '/sprint0' page, where are the UI components are:
+    // Before running each test, navigate to the '/sprint1' page, where are the UI components are:
     beforeEach(() => {
       cy.visit('/sprint1')
     })
+
+
+    // Here's a group of tests which check to make sure that the text area for putting in a number works
+  describe('Number input', () => {
+    // Make sure that all of the important elements are displaying correctly:
+    it('Should have a text input field', () => {
+      // Check the text field:
+      cy.get('newComponent')
+        .should('have.class', 'newComponent');
+    })
+  })
 });
+
