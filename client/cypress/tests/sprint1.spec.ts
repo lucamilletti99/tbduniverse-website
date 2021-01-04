21e0365c69f1a71c70bc58acb38ea5d95260be53
unknown <baw221@lehigh.edu>
2021-01-03 22:45:09 -0500

    unit test page created, no frontend tests right now just navigates to page

---
+++ client/cypress/tests/sprint1.spec.ts
@@ -0,0 +1,6 @@
+describe('Sprint 1', () => {
+    // Before running each test, navigate to the '/sprint0' page, where are the UI components are:
+    beforeEach(() => {
+      cy.visit('/sprint1')
+    })
+});
