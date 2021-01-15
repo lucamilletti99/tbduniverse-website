1c44334e8929796f34c6ecba9e4c525ee9882a66
Luca Milletti <lpm222@lehigh.edu>
2021-01-14 14:14:03 -1000

    Adding it statements

--- client/cypress/tests/sprint2.spec.ts
+++ client/cypress/tests/sprint2.spec.ts
@@ -1,5 +1,10 @@
 describe('Sprint 2', () => {
-    describe('button clicks', () =>{
-        
-    }
+    describe('button clicks', () => {
+        it('Should output a joke', () => {
+
+        });
+        it('Should output "User Doesnt like Jokes"', () => {
+
+        });
+    })
 });
