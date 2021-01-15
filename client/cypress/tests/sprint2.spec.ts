c0cb54c3a50ac978d5553ef886ff686c7dc2ba47
Luke Lenny <lml222@lehigh.edu>
2021-01-14 19:53:30 -0500

    unit test?

--- client/cypress/tests/sprint2.spec.ts
+++ client/cypress/tests/sprint2.spec.ts
@@ -1,7 +1,21 @@
 describe('Sprint 2', () => {
     describe('button clicks', () => {
-        it('Should output a joke', () => {
 
+        //Luke Lenny's unit test, checking to ensure the Yes button correctly requests 
+        //from server and recieves a joke
+        it('Should output a joke', () => { 
+          cy.fixture('jokeChoice.json').then(joke =>{
+            cy.intercept('GET', '/api/jokes', joke); 
+
+            cy.reload();
+
+            cy.get('card-header')
+              cy.should('have.length', joke.length)
+              .then(headers =>{
+                 expect(headers[0].innerText).to.equal(joke.choice)
+              });
+          });
+           
         });
         it('Should output "User Doesnt like Jokes"', () => {
 
