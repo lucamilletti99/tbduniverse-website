cafb29a45c4a53bb6192de5b4d4726708b1e5629
Luca Milletti <lpm222@lehigh.edu>
2021-01-23 11:21:40 -0800

    Making page

--- client/src/pages/Sprint3Page.vue
+++ client/src/pages/Sprint3Page.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page page--about">
     <div class="alert alert-danger" role="alert">
-      This part will be done by you!
+      This part will done by you!
     </div>
     <h1>{{ title }}</h1>
     <div class="btn-group" role="group" aria-label="Basic example">
@@ -9,26 +9,42 @@
       <a v-bind:href="slackLink" target="_blank" class="btn btn-primary">Slack</a>
       <a v-bind:href="descLink" target="_blank" class="btn btn-primary">Project Description</a>
       <a v-bind:href="coursesiteLink" target="_blank" class="btn btn-primary">Coursesite</a>
+      <a v-bind:href="tbd" target="_blank" class="btn btn-primary"> <i class="fas fa-dumpster-fire fa-3x"></i> </a>  
+      <a v-bind:href="clockLink" target="_blank" class="btn btn-primary">World Clock</a>
     </div>
-    <p class="mt-3">{{ description }}</p>
+    <p>{{ description }}</p>
+    <p>{{ description2 }}</p>
+      <label>Do you like jokes?</label>
+      <Jokes></Jokes><br><br><br>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import newComponent from '../components/newComponent.vue'
+import formComponent from '../components/formComponent.vue'
+import Jokes from '../components/Jokes.vue'
 
-@Component
-export default class Sprint1Page extends Vue {
-  private title: string = 'Sprint Three: Working with your users.';
-  private description: string = 'The final sprint is about adding the details needed to meet your users needs. This includes adding authentication, and implementing an authorization rule. It also includes integrating a cloud-computing service to implement additional app functionality. Finally, you will publish your project publicly using a hosting service.';
+@Component({
+  components: {
+    newComponent,
+    Jokes
+  }
+})
 
-  private rubricLink: string = 'https://docs.google.com/document/d/1tmB0vWYyzrfBP0OmJcrpvNmTSm6az5x_mA3LnijLUjo/edit?usp=sharing';
-  private slackLink: string = 'https://app.slack.com/client/T01HDFMDT0T/C01GYGE27M5';
+export default class Sprint2Page extends Vue {
+  private title: string = 'Sprint Two: Working Better With Your Teammates';
+  private description: string = 'Sprint 1: The Frontend has utilized the Font Awesome CDN and imported the dumpster fire emoji (added a link aswell) and this code symbol';
+  private description2: string = 'Sprint 2: Added a user choice for joke sent to backend (and a cheeky world clock!) . Our main goal was to ensure our get endpoint was working smoothly, and that the cookies and caching was being handled correctly!';
+  private rubricLink: string = 'https://docs.google.com/document/d/1zVWKnUkdp5YZLm7QTdsexhSakfIsVgAkUdsRtXHgYRc/edit?usp=sharing';
+  private slackLink: string = 'https://app.slack.com/client/T01HDFMDT0T/C01J352F8U8';
   private descLink: string = 'https://docs.google.com/document/d/1igm6ORVncy5TLJkQsHFqOr72ojPIQ7bRpjF-H4ss0GY/edit?usp=sharing';
   private coursesiteLink: string = 'https://coursesite.lehigh.edu/course/view.php?id=195061';
+  private tbd: string = 'http://tbd.world/';
+  private clockLink: string = 'https://www.timeanddate.com/worldclock/';
 }
-</script>
 
+</script>
 <style scoped>
   .about-logo a {
     padding: 20px;
@@ -39,3 +55,6 @@ export default class Sprint1Page extends Vue {
     opacity: 0.8;
   }
 </style>
+newComponent{
+  color: red;
+}
